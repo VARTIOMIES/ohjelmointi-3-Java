2cc8e072577ce6fd39f30d6179c3e66cca0bc697
Onni Merilä <onni.merila@gmail.com>
2022-02-09 19:34:37 +0200

    Aloitettu 5.7 tekemistä

---
+++ Round5/wordgame/WordGame.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Onni Merila
+ */
+public class WordGame {
+    
+    public WordGame(String wordFilename){
+        
+    }
+    
+    public void initGame(int wordIndex, int mistakeLimit){
+        
+    }
+    
+    public boolean isGameActive(){
+        
+    }
+    
+    public WordGameState getGameState(){
+        
+    }
+    
+    public WordGameState guess(String word){
+        
+    }
+    
+    public static class WordGameState{
+        private String word;
+        private int mistakes;
+        private int mistakeLimit;
+        private int missingChars;
+        
+        
+        
+        public String getWord(){
+            
+        }
+        
+        public int getMistakes(){
+            
+        }
+        
+        public int getMistakeLimit(){
+            
+        }
+        
+        public int getMissingChars(){
+            
+        }
+        
+        
+        
+    }
+}
