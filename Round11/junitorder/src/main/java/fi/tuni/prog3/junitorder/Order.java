e72a340c46029edd98a4ab22deb0ac3ecb09eaca
Onni Merilä <onni.merila@gmail.com>
2022-03-22 18:18:00 +0200

    11.3 junitorder: Created tester class from info from the given documentation.

---
+++ Round11/junitorder/src/main/java/fi/tuni/prog3/junitorder/Order.java
@@ -0,0 +1,46 @@
+
+import java.util.List;
+
+package fi.tuni.prog3
+public class Order {
+
+    public boolean addItems(Item item, int count){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public List<Entry> getEntries(){
+
+    }
+
+    public int getEntryCount(){
+
+    }
+
+    public int getItemCount(){
+
+    }
+
+    public double getTotalPrice(){
+
+    }
+
+    public void removeItems(String name, int count){
+
+    }
+
+    public class Item{
+
+        public Item(String name, double price){
+
+        }
+
+    }
+
+    public class Entry{
+
+    }
+}
---
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTest.java
@@ -0,0 +1,174 @@
+
+package fi.tuni.prog3.junitorder;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+
+public class OrderTest {
+
+    @Test
+    public void testAddItemsWithItem(){
+
+        Order order = new Order();
+        Order.Item item = new Order.Item("Peruna",2.5);
+        int count = 1;
+
+        if (order.addItems(item,count)){
+            assertFalse(order.isEmpty());
+        }
+        assert true;
+    }
+    @Test
+    public void testAddItemsWithString(){
+
+        Order order = new Order();
+        String name = "Peruna";
+        Order.Item item = new Order.Item(name,1.0);
+        order.addItems(item,1);
+
+        assertTrue(order.addItems(name,1));
+
+    }
+
+    @Test
+    public void testGettersModifying(){
+        Order order = new Order();
+
+        order.addItems(new Order.Item("porkkana",2.5),10);
+
+        assertEquals(order.getEntries(),order.getEntries());
+        assertEquals(order.getEntryCount(),order.getEntryCount());
+        assertEquals(order.getItemCount(),order.getItemCount());
+    }
+
+    @Test
+    public void testPricing(){
+        Order order = new Order();
+
+        // Create items
+        Order.Item item1 = new Order.Item("peruna",5.5);
+        Order.Item item2 = new Order.Item("porkkana",3.3);
+        Order.Item item3 = new Order.Item("jooseppi",25.2);
+
+        // Add items
+        order.addItems(item1,5);
+        order.addItems(item2,4);
+        order.addItems(item3,1);
+
+        // Entries
+        Order.Entry entry = new Order.Entry(item2,4);
+
+        // Is the pricing correct?
+        double expectedPrice = 5*5.5 + 4*3.3 + 25.2;
+        assertEquals(expectedPrice,order.getTotalPrice());
+        assertEquals(5.5,item1.getPrice());
+
+        assertEquals(3.3,entry.getUnitPrice());
+
+    }
+
+    @Test
+    public void testEmptiness(){
+        Order order = new Order();
+        assertEquals(order.getItemCount() == 0,order.isEmpty());
+
+        order.addItems(new Order.Item("penis",2.4),2);
+
+        assertEquals(order.getItemCount()!=0,!order.isEmpty());
+
+    }
+
+    @Test
+    public void testRemovingItem(){
+        Order order = new Order();
+
+        Order.Item item1 = new Order.Item("peruna",5.5);
+
+        order.addItems(item1,5);
+
+        order.removeItems("peruna",3);
+        assertEquals(3, order.getItemCount());
+
+        order.removeItems("peruna",2);
+        assertTrue(order.isEmpty());
+
+    }
+
+
+    @Test
+    public void testGetItemName(){
+        String name = "Peruna";
+
+        // Test both name-getters
+
+        Order.Item item = new Order.Item(name,4);
+        assertEquals(name,item.getName());
+        Order.Entry entry = new Order.Entry(item,1);
+        assertEquals(name,entry.getItemName());
+
+    }
+
+    @Test
+    public void testEquals(){
+        Order.Item item1 = new Order.Item("Peruna",2);
+        Order.Item item2 = new Order.Item("Peruna",1);
+
+        assertEquals(item1, item2);
+        assertTrue(item1.equals(item2));
+
+    }
+
+    @Test
+    public void testToString(){
+        Order.Item item = new Order.Item("Peruna",2.1);
+        Order.Entry entry = new Order.Entry(item,2);
+
+        assertEquals("Item(Peruna,2.10",item.toString());
+        assertEquals("2 units of Peruna",entry.toString());
+
+    }
+
+    @Test
+    public void testExceptionThrowing(){
+
+        Order.Item item1 = new Order.Item("Peruna",2.59);
+        Order.Item item2 = new Order.Item("Peruna",4.66);
+
+        Order order1 = new Order();
+        assertThrows(IllegalArgumentException.class,()->order1.addItems(item1,-1));
+
+        Order order2 = new Order();
+        order2.addItems(item1,2);
+        assertThrows(IllegalStateException.class,()->order2.addItems(item2,4));
+
+        Order order3 = new Order();
+        order3.addItems(item1,3);
+        assertThrows(NoSuchElementException.class,()->order3.addItems("Jooseppi",2));
+        assertThrows(IllegalArgumentException.class,()->order3.addItems("Peruna",-1));
+
+        Order order4 = new Order();
+        order4.addItems(item1,3);
+        assertThrows(IllegalArgumentException.class,()->order4.removeItems("Peruna",-1));
+        assertThrows(IllegalArgumentException.class,()->order4.removeItems("Peruna",4));
+        assertThrows(NoSuchElementException.class,()->order4.removeItems("Jooseppi",2));
+
+
+        assertThrows(IllegalArgumentException.class,()-> new Order.Item(null,1));
+        assertThrows(IllegalArgumentException.class,()->new Order.Item("Kukka",-1.0));
+
+        assertThrows(IllegalArgumentException.class,()-> new Order.Entry(item1,-1));
+
+    }
+
+
+
+
+}
